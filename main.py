757d9e504503af4869e943441d29ace38a898be9
MarieSZgurich <mszguric@mtu.edu>
2022-07-22 09:50:20 -0700

    Wrestling VSCode, I'll test this later

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import soundfile as sf # reads audio data
 import numpy as np # handles audio data
-from pydub import AudioSegment
-from pydub.playback import play # plays audio file
+from playsound import playsound # plays audio file
 from multiprocessing import Process # let audio and visual work simultaneously
 
+
+# import test song
+# don't worry, I made this. No copyright claims ;)
+
+playsound('test.mp3')
