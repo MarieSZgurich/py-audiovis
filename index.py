e949c4db4fd6c61e00cd082e94c4af77a284eee6
MarieSZgurich <mszguric@mtu.edu>
2022-07-22 08:06:22 -0700

    Importing the bare necessities

---
+++ index.py
@@ -0,0 +1,6 @@
+import soundfile as sf # reads audio data
+import numpy as np # handles audio data
+from pydub import AudioSegment
+from pydub.playback import play # plays audio file
+from multiprocessing import Process # let audio and visual work simultaneously
+
